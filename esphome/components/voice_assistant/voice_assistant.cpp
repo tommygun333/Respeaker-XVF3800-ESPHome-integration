b275aed383951f90f0c673b4bfe779f799015208
tommygun333 <tomas.hlavko@gmail.com>
2026-09-13 22:34:45 +0200

    Update voice_assistant.cpp

--- esphome/components/voice_assistant/voice_assistant.cpp
+++ esphome/components/voice_assistant/voice_assistant.cpp
@@ -755,7 +755,7 @@ void VoiceAssistant::signal_stop_() {
 }
 
 void VoiceAssistant::start_playback_timeout_() {
-  this->set_timeout("playing", 2000, [this]() {
+  this->set_timeout("playing", 6000, [this]() {
     this->cancel_timeout("speaker-timeout");
     this->set_state_(State::RESPONSE_FINISHED, State::RESPONSE_FINISHED);
 
